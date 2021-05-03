705242c2e2672c2802fbc40fc1ef23a36ca2f60c
Valium-K <lschan96@gmail.com>
2021-05-03 17:26:18 +0900

    l1

---
+++ src/main/java/com/example1/springlecture/SpringLectureApplication.java
@@ -0,0 +1,14 @@
+package com.example1.springlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLectureApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringLectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example1/springlecture/SpringLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example1.springlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
