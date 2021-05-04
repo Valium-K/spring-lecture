fc7eaf86fe192400543c857acf8388b490b6d438
Valium-K <lschan96@gmail.com>
2021-05-04 12:54:18 +0900

    responseBody

--- src/main/java/com/example1/springlecture/controller/HelloController.java
+++ src/main/java/com/example1/springlecture/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.example1.springlecture.controller;
 
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +14,43 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello");
         return "hello";
+    }
+
+    // mvc방식 - template엔진 이용
+    @GetMapping("hello-mvc")
+    public String helloMve(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";    // controller 가 viewRsolver 에 return 한다.
+    }
+
+    // API 방식1 - data를 그대로 내려줌 즉 해당 uri로 접근하면 html 태그없이 hello null 만 나온다.
+    @GetMapping("hello-string")
+    @ResponseBody   // http의 body부에 해당 return을 직접 넣어준다. spring에서는 기본적으로 json 형태로 반환한다.
+    public String helloString(@RequestParam(value = "name", required = false) String name) {
+        return "hello " + name; // @ResponseBody 때문에 viewResolver가 아닌 http 바디부에 그대로 넣어 return한다.
+    }
+
+    // API 방식2 - 이번엔 Hello 객체를 넘겨줘본다. 접근시 json 파일을 받을 수 있다.
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        // @ResponseBody 때문에 viewResolver가 아닌 http 바디부에 그대로 넣어 return한다.
+        // 이 때 String이 아닌 객체이기에 HttpMessageConverter가 json으로 변환 후 return한다.
+        return hello;
+    }
+
+    public static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
---
+++ 요약/@ResponseBody/ResponseBody.md
@@ -0,0 +1,10 @@
+@ResponseBody 사용 원리
+====================
+<img src="/responsebody.png" width="450px" height="300px" alt="responsebody.png"></img><br/>
+
+* `@ResponseBody`를 사용
+	* HTTP의 BODY에 문자 내용을 직접 반환
+	* `ViewResolver` 대신에 `HttpMessageConverter`가 동작
+	* 기본 문자처리: `StringHttpMessageConverter`
+	* 기본 객체처리:  `MappingJackson2HttpMessageconverter`
+	* byte처리 등 기타 여러 `HttpMessageConverter`가 기본적으로 등록되어 있음
